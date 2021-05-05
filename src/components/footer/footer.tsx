4836becf13e76deb9b79e1da907234f376161d75
Austin Terry <pdkkid1@gmail.com>
2021-05-05 11:44:59 -0600

    Logic done?

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,10 +2,9 @@ import styled from "styled-components";
 
 const SourceLink = styled.a`
   user-select: none;
-  font-size: 1em;
-  font-weight: 400;
-  justify-self: left;
-  color: #ecf0f1;
+  color: #CFD7C7;
+  font-size: 2em;
+  font-weight: 600;
   text-decoration: none;
   &:hover {
     color: #ddffbc;
